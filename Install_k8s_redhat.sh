4e5ea310d8509e1308b6a21c567a1df5858f4613
Pathmanathanb <pathmanathanb008@gmail.com>
2025-12-04 19:14:19 +0530

    Update Install_k8s_redhat.sh

--- Install_k8s_redhat.sh
+++ Install_k8s_redhat.sh
@@ -56,7 +56,7 @@ sudo modprobe overlay
 sudo modprobe br_netfilter
 
 # sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k9s.conf
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
@@ -159,4 +159,5 @@ echo "--------------------------------------------------------"
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
+
 source ~/.bashrc
